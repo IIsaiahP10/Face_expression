898de8adfa9e06acf44addcaccc52ed8da7d9cd5
Tamarind0 <78615030+Tamarind0@users.noreply.github.com>
2022-05-09 22:28:56 -0700

    graphed current data

--- FaceExpressionDetection.py
+++ FaceExpressionDetection.py
@@ -26,10 +26,40 @@ for file in files:
     #cv2.imshow(str(file), fileImages)
     #cv2.waitKey()
     #print(dominant_Emotion, emotion_Score)
-print('made it outside the loop')
-x = np.linspace(1,14,13)
+
+x = np.linspace(1,17, num= 17) # total frames
 y = emotion_Score_List# time passed between the time each frame was taken
 
-plt.plot(x,y, label = 'Teachers Emotions Over Time')
-plt.show
-print('graphed')
+plt.plot(x,y, 'o', ms=5)
+plt.title( 'Teachers Emotions Over Time')
+plt.xlabel("Frames")
+plt.ylabel("Confidenct in Dominant Expression")
+for x in range(len(emotion_Score_List)):
+  plt.text(x, emotion_Score_List[x], captured_emotions_List[x], fontsize = 8)    
+plt.show()
+###Student 1
+captured_emotions_List.clear()
+emotion_Score_List.clear()
+path1 = 'Known_Images/Student_1'
+files = Path(path1).glob('*')
+for file in files:
+    fileImages = cv2.imread(str(file))
+    # using the FER trained model to detect an emotion in the image
+    emotion_detector = FER(mtcnn=True)
+    # Capture all the emotions on the image
+    captured_emotions = emotion_detector.detect_emotions(fileImages)
+    # Use the top Emotion() function to call for the dominant emotion in the image
+    dominant_Emotion, emotion_Score = emotion_detector.top_emotion(fileImages)
+    captured_emotions_List.append(dominant_Emotion)
+    emotion_Score_List.append(emotion_Score)
+x = np.linspace(1,16, num= 16) # total frames
+y = emotion_Score_List# time passed between the time each frame was taken
+
+plt.plot(x,y, 'o', ms=5)
+plt.title( 'Teachers Emotions Over Time')
+plt.xlabel("Frames")
+plt.ylabel("Confidenct in Dominant Expression")
+for x in range(len(emotion_Score_List)):
+  plt.text(x, emotion_Score_List[x], captured_emotions_List[x], fontsize = 8)    
+plt.show()
+
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-test_image_one = cv2.imread(path1)
-# emo_detector = FER(mtcnn=True)
-# # Capture all the emotions on the image
-# captured_emotions = emo_detector.detect_emotions(test_image_one)
