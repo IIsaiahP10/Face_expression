a7598f375ee931605899936e2a4831d19d31f860
Tamarind0 <78615030+Tamarind0@users.noreply.github.com>
2022-05-12 14:31:46 -0700

    Final changes to FaceExpressionDetection

--- FaceExpressionDetection.py
+++ FaceExpressionDetection.py
@@ -1,7 +1,9 @@
 
 from cProfile import label
 from pathlib import Path
+from pickle import DICT
 from tkinter import Image
+from typing import Dict
 from fer import FER
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -9,7 +11,7 @@ import cv2
 import os
 
 def DetectFaceExpressions(foldername,name, size):
-
+  Dict = {'angry': 0, 'disgust': 0, 'fear': 0, 'happy': 0, 'sad': 0,'surprise':0, 'neutral':0}
   captured_emotions_List = []
   emotion_Score_List = []
   captured_emotions_List.clear()
@@ -18,12 +20,17 @@ def DetectFaceExpressions(foldername,name, size):
   files = Path(path1).glob('*')
   for file in files:
       fileImages = cv2.imread(str(file))
-      # using the FER trained model to detect an emotion in the image
+      # using the FER trained model to detect an emotion in the image and using MTCNN
       emotion_detector = FER(mtcnn=True)
-      # Capture all the emotions on the image
-      captured_emotions = emotion_detector.detect_emotions(fileImages)
-      # Use the top Emotion() function to call for the dominant emotion in the image
+
+      # To capture all the emotions from the images uncomment the lines below
+      # captured_emotions = emotion_detector.detect_emotions(fileImages)
+      # print(captured_emotions)
+
+      # Using the top Emotion() function to call for the dominant emotion in the image
       dominant_Emotion, emotion_Score = emotion_detector.top_emotion(fileImages)
+      if(dominant_Emotion != None):
+        Dict[dominant_Emotion] += 1
       captured_emotions_List.append(dominant_Emotion)
       emotion_Score_List.append(emotion_Score)
       # To see the images being analyzed for their emotion and score, uncomment the lines below
@@ -33,14 +40,21 @@ def DetectFaceExpressions(foldername,name, size):
   x = np.linspace(1,size, num= size) # total frames
   y = emotion_Score_List# time passed between the time each frame was taken
 
+  max = Dict.get('angry')
+  averageEmotion = ""
+
+  for key in Dict:
+    if(Dict.get(key)) > max:
+      max = Dict.get(key)
+      averageEmotion = key
   plt.plot(x,y, 'o', ms=5)
   plt.title( name +' Emotions Over Time')
   plt.xlabel("Frames")
   plt.ylabel("Confidenct in Dominant Expression")
   for x in range(len(emotion_Score_List)):
-    plt.text(x, emotion_Score_List[x], captured_emotions_List[x], fontsize = 8)    
+    plt.text(x, emotion_Score_List[x], captured_emotions_List[x], fontsize = 8) 
+  plt.text(5, 0.4, 'Avg: '+ averageEmotion, fontsize = 14)  
   plt.show()
-
 
 
 DetectFaceExpressions("teacher","Teachers", 17)
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-DetectFaceExpressions("teacher","Teachers", 17)
-# DetectFaceExpressions("Student_1","Student1", 16)
